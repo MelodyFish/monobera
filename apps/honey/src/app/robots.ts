dccb2fc423d2d3020cae82762ded7ab8386055e6
yuguoqiang <yuguoqiang@hygon.cn>
2024-12-28 14:54:48 +0800

    refactor: using computed properties to robots.ts

--- apps/honey/src/app/robots.ts
+++ apps/honey/src/app/robots.ts
@@ -2,19 +2,10 @@ import { MetadataRoute } from "next";
 import { isIPFS } from "@bera/config";
 
 export default function robots(): MetadataRoute.Robots {
-  if (isIPFS || process.env.DONT_INDEX === "1") {
-    return {
-      rules: {
-        userAgent: "*",
-        disallow: "/",
-      },
-    };
-  }
-
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      [isIPFS || process.env.DONT_INDEX === "1" ? "disallow" : "allow"]: "/",
     },
   };
 }
